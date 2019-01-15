5452036ce934ac0609cd921767d8a43070444a5f
Grant Weiss <grant.sweiss@gmail.com>
2019-01-15 11:04:04 -0600

    add pwd condition

--- bash.js
+++ bash.js
@@ -4,8 +4,12 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); // remove the newline
-
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+  if (cmd === 'pwd') {
+    process.stdout.write(`Current directory: ${process.cwd()}`)
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
 });
+
 
