e41cf821b6df4d0f39fc58b2562d531bbb54b377
Tori Pope <toricpope@gmail.com>
2019-01-15 11:54:21 -0600

    cat work

--- bash.js
+++ bash.js
@@ -4,12 +4,17 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); // remove the newline
-  if (cmd === 'pwd') {
+  cmd.split(' ');
+
+  if (cmd[0] === 'pwd') {
     const pwd = require('./pwd');
     pwd();
-  } else if (cmd === 'ls'){
+  } else if (cmd[0] === 'ls'){
       const ls = require('./ls');
       ls();
+  } else if (cmd[0] === 'cat') {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
   } else {
       process.stdout.write('You typed: ' + cmd);
       process.stdout.write('\nprompt > ');
